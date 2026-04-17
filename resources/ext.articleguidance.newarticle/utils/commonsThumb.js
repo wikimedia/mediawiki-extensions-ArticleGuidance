37bed22ff4f00b8a637422fcb72ef1f09f9feb94
Sbisson <sbisson@wikimedia.org>
2026-04-17 14:59:33 -0400

    Use standard 60px thumbnail
    Change-Id: Ibc498e504a7f56e4ddbf7f734d0821ea798bef7f

--- resources/ext.articleguidance.newarticle/utils/commonsThumb.js
+++ resources/ext.articleguidance.newarticle/utils/commonsThumb.js
@@ -1,17 +1,24 @@
 const { md5 } = require( './md5.js' );
 
 /**
+ * 60 is one of the officially supported thumbnail size.
+ * See https://www.mediawiki.org/wiki/Common_thumbnail_sizes
+ * Is it important to use a standard size to avoid causing
+ * extra load on the infrastructure.
+ * The thumb is displayed as 48x46 so 60 makes sense.
+ */
+const THUMB_WIDTH = 60;
+
+/**
  * Convert a Wikimedia Commons image filename to a thumbnail URL.
  *
  * Replicates the PHP WikidataInfoFetcher::getCommonsImageUrl() logic client-side,
  * using the same MD5-based directory path that Wikimedia's file storage uses.
  *
  * @param {string} filename Image filename from a Wikidata P18 claim
- * @param {number} [width=200] Thumbnail width in pixels
  * @return {string} Thumbnail URL
  */
-function getCommonsThumbUrl( filename, width ) {
-	const w = width || 200;
+function getCommonsThumbUrl( filename ) {
 	const normalized = filename.replace( / /g, '_' );
 	const bytes = new TextEncoder().encode( normalized );
 	const hash = md5( bytes );
@@ -24,7 +31,7 @@ function getCommonsThumbUrl( filename, width ) {
 		.replace( /\)/g, '%29' )
 		.replace( /\*/g, '%2A' );
 	return 'https://upload.wikimedia.org/wikipedia/commons/thumb/' +
-		dir1 + '/' + dir2 + '/' + encoded + '/' + w + 'px-' + encoded;
+		dir1 + '/' + dir2 + '/' + encoded + '/' + THUMB_WIDTH + 'px-' + encoded;
 }
 
 module.exports = { getCommonsThumbUrl };
